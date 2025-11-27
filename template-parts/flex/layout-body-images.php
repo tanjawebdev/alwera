eb31d52a3908591899c542fe0a88d1dd2670a912
Tanja Kobler <kobler.tanja@gmail.com>
2025-11-27 21:44:27 +0100

    add body images

--- template-parts/flex/layout-body-images.php
+++ template-parts/flex/layout-body-images.php
@@ -3,10 +3,12 @@ $images = get_sub_field('images');
 ?>
 <?php if ($images): ?>
   <div class="body-images">
-    <div class="row g-4">
+    <div class="row">
       <?php foreach ($images as $img): ?>
-        <div class="col-md-4">
-          <?php echo wp_get_attachment_image($img['ID'], 'medium', false, ['class' => 'img-fluid']); ?>
+        <div class="body-image col-12 col-md-6">
+          <picture>
+            <?php echo wp_get_attachment_image($img['ID'], 'medium', false, ['class' => 'img-fluid']); ?>
+          </picture>
         </div>
       <?php endforeach; ?>
     </div>
