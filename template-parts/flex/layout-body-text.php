5de88954eca8f8e1ca080e692f3204e30239dba4
th-zen <mrtacocat3000@gmail.com>
2025-12-02 00:22:41 +0100

    finish text, statement, download components
    also changes to the json: text are to wysiwyg

--- template-parts/flex/layout-body-text.php
+++ template-parts/flex/layout-body-text.php
@@ -1,9 +1,25 @@
 <?php
 $text = get_sub_field('text');
-$alignment = get_sub_field('alignment');
+
+// IMPORTANT: Replace 'section_container_size' with the actual field name 
+// you use to select the container (e.g., 'container_type', 'wrapper_size', etc.)
+// You may need to fetch this from a parent/ancestor field.
+$section_container_size = get_sub_field('container'); // Placeholder - adjust this line
+
+// 1. Determine if the large column class should be used
+$use_lg_cols = ($section_container_size === 'container-lg'); 
+
+// 2. Build the column class based on the container size
+$column_class = $use_lg_cols ? 'col-lg-6' : 'col-12'; 
+
+// 3. Build the offset class, only applying it if we are using the large column size AND alignment is middle
+$offset_class = ($use_lg_cols && $alignment === 'middle') ? 'offset-lg-3' : '';
+
+// 4. Combine all classes
+$classes = trim("$column_class $offset_class");
 ?>
 
-<div class="col-lg-6 <?php echo ($alignment === 'middle') ? 'offset-lg-3' : ''; ?>">
+<div class="<?php echo $classes; ?>">
   <?php if ($text): ?>
     <div class="body-text rte">
       <?php echo wp_kses_post($text); ?>
--- template-parts/flex/layout-downloadlink.php
+++ template-parts/flex/layout-downloadlink.php
@@ -2,11 +2,15 @@
 $text = get_sub_field('text');
 $downloadlink = get_sub_field('downloadlink');
 ?>
-<?php if ($text && $downloadlink): ?>
-  <div class="downloadlink">
-    <a href="<?php echo esc_url($downloadlink); ?>" class="btn btn-primary" download>
-      <?php echo esc_html($text); ?>
-    </a>
-  </div>
+
+<div class="download-wrapper">
+<?php if ($text): ?>
+  <h2><?php echo esc_html($text); ?></h2>
 <?php endif; ?>
 
+<?php if ($downloadlink): ?>
+  <div class="downloadlink">
+    <a href="<?php echo esc_url($downloadlink); ?>" class="btn btn-primary" download>Download</a>
+  </div>
+<?php endif; ?>
+</div>
