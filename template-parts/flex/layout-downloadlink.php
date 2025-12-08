e2bb1a1be5ed517f9328088acd36d28d1142df0c
th-zen <mrtacocat3000@gmail.com>
2025-12-08 21:02:37 +0100

    dont show entire download link if there is no file

--- template-parts/flex/layout-downloadlink.php
+++ template-parts/flex/layout-downloadlink.php
@@ -4,12 +4,13 @@ $file_field = get_field('my_download_file');
 $file_url = $file_field['url'];
 ?>
 
-<div class="download-wrapper">
-  <?php if ($text): ?>
-    <h2><?php echo esc_html($text); ?></h2>
-  <?php endif; ?>
+<?php if ($file_url): ?>
+  <div class="download-wrapper">
+    <?php if ($text): ?>
+      <h2><?php echo esc_html($text); ?></h2>
+    <?php endif; ?>
 
-  <?php if ($file_url): ?>
+
     <div class="downloadlink">
       <a href="<?php echo esc_url($file_url); ?>" class="btn btn-primary" download>Download</a>
     </div>
