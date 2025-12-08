62665c6af0a53a383f1557f3c82f8d157383c318
th-zen <mrtacocat3000@gmail.com>
2025-12-08 20:13:01 +0100

    add subtitle_tag php code

--- template-parts/flex/layout-statement.php
+++ template-parts/flex/layout-statement.php
@@ -2,14 +2,15 @@
 $title = get_sub_field('title');
 $subtitle = get_sub_field('subtitle');
 $direction = get_sub_field('direction');
+$subtitle_tag_name = $subtitle_tag ? esc_attr($subtitle_tag) : 'p';
 ?>
 
-  <div class="statement statement-<?php echo esc_attr($direction ?: 'top'); ?>">
-    <?php if ($title): ?>
-      <h2><?php echo esc_html($title); ?></h2>
-    <?php endif; ?>
-    
-    <?php if ($subtitle): ?>
-      <p class="subtitle"><?php echo esc_html($subtitle); ?></p>
-    <?php endif; ?>
-  </div>
+<div class="statement statement-<?php echo esc_attr($direction ?: 'top'); ?>">
+  <?php if ($title): ?>
+    <h2><?php echo esc_html($title); ?></h2>
+  <?php endif; ?>
+
+  <?php if ($subtitle): ?>
+    <<?php echo $subtitle_tag_name; ?> class="subtitle"><?php echo esc_html($subtitle); ?></<?php echo $subtitle_tag_name; ?>>
+  <?php endif; ?>
+</div>
