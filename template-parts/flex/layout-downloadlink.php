46077cb81e478139e102143b8d626dad423329b5
th-zen <mrtacocat3000@gmail.com>
2025-12-08 21:00:34 +0100

    make downloadlink work with acf file

--- template-parts/flex/layout-downloadlink.php
+++ template-parts/flex/layout-downloadlink.php
@@ -1,16 +1,17 @@
 <?php
 $text = get_sub_field('text');
-$downloadlink = get_sub_field('downloadlink');
+$file_field = get_field('my_download_file');
+$file_url = $file_field['url'];
 ?>
 
 <div class="download-wrapper">
-<?php if ($text): ?>
-  <h2><?php echo esc_html($text); ?></h2>
-<?php endif; ?>
+  <?php if ($text): ?>
+    <h2><?php echo esc_html($text); ?></h2>
+  <?php endif; ?>
 
-<?php if ($downloadlink): ?>
-  <div class="downloadlink">
-    <a href="<?php echo esc_url($downloadlink); ?>" class="btn btn-primary" download>Download</a>
-  </div>
-<?php endif; ?>
+  <?php if ($file_url): ?>
+    <div class="downloadlink">
+      <a href="<?php echo esc_url($file_url); ?>" class="btn btn-primary" download>Download</a>
+    </div>
+  <?php endif; ?>
 </div>
