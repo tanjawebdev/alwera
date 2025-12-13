a324fd0953b79b911f7ee584e4910523bd927a51
th-zen <mrtacocat3000@gmail.com>
2025-12-13 20:43:48 +0100

    margin changes to text-right-image-left

--- template-parts/flex/layout-text-rights-image-left.php
+++ template-parts/flex/layout-text-rights-image-left.php
@@ -20,7 +20,7 @@ $image = get_sub_field('image');
     <?php endif; ?>
 
     <?php if ($title): ?>
-      <h2><?php echo esc_html($title); ?></h2>
+      <h2 class="image-text-h2"><?php echo esc_html($title); ?></h2>
     <?php endif; ?>
 
     <?php if ($text): ?>
