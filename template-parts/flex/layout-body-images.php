d7d614098cf4ffb4add0b9141f2132c7bcd0b086
th-zen <mrtacocat3000@gmail.com>
2025-12-14 23:54:23 +0100

    implement horizontal scroll on mobile to body-images

--- template-parts/flex/layout-body-images.php
+++ template-parts/flex/layout-body-images.php
@@ -3,15 +3,18 @@ $images = get_sub_field('images');
 ?>
 <?php if ($images): ?>
   <div class="body-images">
-    <div class="row">
+
+    <div class="d-flex flex-md-wrap horizontal-scroll-strip">
+
       <?php foreach ($images as $img): ?>
-        <div class="body-image col-12 col-md-6">
+        <div class="body-image flex-shrink-0 col-md-6 w-75 w-sm-50 w-md-auto">
           <picture>
             <?php echo wp_get_attachment_image($img['ID'], 'medium', false, ['class' => 'img-fluid']); ?>
           </picture>
         </div>
       <?php endforeach; ?>
+
     </div>
+
   </div>
 <?php endif; ?>
-
