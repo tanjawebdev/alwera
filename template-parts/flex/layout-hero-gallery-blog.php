960e898d46bef67441089c04bfea4186ba46eb1b
th-zen <mrtacocat3000@gmail.com>
2025-12-15 13:38:54 +0100

    disable media controls in hero_hallery on mobile

--- template-parts/flex/layout-hero-gallery-blog.php
+++ template-parts/flex/layout-hero-gallery-blog.php
@@ -7,10 +7,10 @@ $interval = get_sub_field('interval') ?: 3000;
   <div class="hero-gallery typo-white splide" data-interval="<?php echo esc_attr($interval); ?>">
     <div class="splide__track">
       <ul class="splide__list">
-        <?php foreach ($blog_posts as $post_url): 
+        <?php foreach ($blog_posts as $post_url):
           // Convert URL to post ID
           $post_id = url_to_postid($post_url);
-        ?>
+          ?>
           <li class="splide__slide">
             <div class="hero-gallery-item">
               <div class="hero-gallery-content">
@@ -24,7 +24,13 @@ $interval = get_sub_field('interval') ?: 3000;
               </div>
 
               <div class="hero-media">
-                <?php if (has_post_thumbnail($post_id)): ?>
+                <?php if ($video_url): ?>
+                  <video autoplay loop muted playsinline class="hero-video">
+                    <source src="<?php echo $video_url; ?>" type="<?php echo $video_mime; ?>">
+                    Your browser does not support the video tag.
+                  </video>
+
+                <?php elseif (has_post_thumbnail($post_id)): ?>
                   <picture class="hero-picture">
                     <?php echo get_the_post_thumbnail($post_id, 'large', ['class' => 'img-fluid']); ?>
                   </picture>
