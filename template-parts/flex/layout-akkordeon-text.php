ac84036e4fb0dbc989ea0f61c995ceb9064815b9
Tanja Kobler <kobler.tanja@gmail.com>
2026-03-07 12:04:26 +0100

    akkordeon fix

--- template-parts/flex/layout-akkordeon-text.php
+++ template-parts/flex/layout-akkordeon-text.php
@@ -12,11 +12,9 @@ $items = get_sub_field('items');
     <div class="accordion" id="accordionText">
       <?php foreach ($items as $index => $item): ?>
         <div class="accordion-item">
-          
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $index; ?>" aria-expanded="false">
-              <span class="h3"><?php echo esc_html($item['subtitle']); ?></span>
-            </button>
-          
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $index; ?>" aria-expanded="false">
+            <span class="h3"><?php echo esc_html($item['subtitle']); ?></span>
+          </button>
           <div id="collapse<?php echo $index; ?>" class="accordion-collapse collapse" data-bs-parent="#accordionText">
             <div class="accordion-body">
               <?php if ($item['text']): ?>
