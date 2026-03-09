2f2a8ad68cfb36f8db7adc17812755c909615848
Tanja Kobler <kobler.tanja@gmail.com>
2026-03-09 11:38:35 +0100

    add text to before footer

--- template-parts/flex/before-footer.php
+++ template-parts/flex/before-footer.php
@@ -1,11 +1,20 @@
 <section class="before-footer">
   <div class="before-footer-inner container-fluid">
-      <div class="before-footer__left col-12 col-md-4 col-lg-6">
-      <span class="h3">  
-      Mit Unterstützung von Bund, Land und Europäischer Union
-      </span>
+    <div class="before-footer_left-container col-12 col-md-12 col-xl-6">
+      <div class="before-footer__left col-12 col-md-6">
+        <span class="h3">  
+        Mit Unterstützung von Bund, Land und Europäischer Union
+        </span>
       </div>
-    <div class="before-footer__right col-12 col-md-8 col-lg-6">
+
+      <div class="before-footer__center col-12 col-md-5 col-xl-6">
+        <span>  
+        Das Projekt „Kürbiskernölpressen mit dazugehöriger Infrastruktur und Nebenanlagen“ wird im Rahmen des Förderprogramms „LE 14-20 Entwicklung des ländlichen Raums – Verarbeitung, Vermarktung und Entwicklung landwirtschaftlicher Erzeugnisse“ unterstützt. 
+        </span>
+      </div>
+  </div>
+
+    <div class="before-footer__right col-12 col-md-12 col-xl-6">
       <img src="<?php echo get_template_directory_uri(); ?>/static/img/foerderleiste.jpg" alt="Förderleiste" class="before-footer__img">
     </div>
   </div>
